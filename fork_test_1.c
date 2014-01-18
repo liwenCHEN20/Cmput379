d5c91490ce3e331ea4a75ceae570858f2c0caad7
U-Atlas\Maxime <Maxime@Atlas.(none)>
2014-01-18 00:18:09 -0700

    fork_test_1.c - Comments added!

--- fork_test_1.c
+++ fork_test_1.c
@@ -2,8 +2,18 @@
 # include <sys/types.h>
 # include <unistd.h>
 
+
+/*
+ * System call fork() is used to create processes. It takes no arguments and returns 
+ * a process ID. The purpose of fork() is to create a new process, which becomes the
+ * child process of the caller. After a new child process is created, both processes
+ * will execute the next instruction following the fork() system call.
+ */
+
+
 int main(void)
 {
+	fork();
 	printf("Hello\n");
 	fork();
 	printf("Bye\n");
